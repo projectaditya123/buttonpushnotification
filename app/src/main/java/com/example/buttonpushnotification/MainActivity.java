0f5f27e6a9f65ffc9f0b5156f801bd5de5e3b5fe
miftakhuljannan <miftakhuljannan04@gmail.com>
2023-05-05 16:17:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/buttonpushnotification/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.buttonpushnotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    }
